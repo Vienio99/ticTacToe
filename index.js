8872ace653fae52ce814919da4a33c85174a181b
Vienio99 <kyve96@gmail.com>
2021-04-30 21:05:18 +0200

    added function to populate board object

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const gameBoard = (player, square) => {
-    const board = {};
-}
+const gameBoard = (() => {
+    let board = {};
+
+    const populateBoard = (field, mark) => {
+        board[field] = mark;
+    };
+
+    const checkEnd = () => {
+
+    }
+    
+    return { board, populateBoard, checkEnd };
+})();
 
 
 
@@ -15,24 +25,24 @@ const flowControl = (() => {
 
     const player1 = Player('Player1', 'X');
     const player2 = Player('Player2', 'O');
+
     const markP1 = player1.getMark();
     const markP2 = player2.getMark();
     let currentMark = markP1;
-    console.log(squares)
 
     Array.from(squares).forEach((square) => {
         square.addEventListener('click', () => {
             const chosenSquare = document.getElementById(`${square.id}`);
+            let squareTextBox = chosenSquare.firstElementChild;
 
-            switch (currentMark) {
-                case markP1:
-                    chosenSquare.firstElementChild.textContent = markP1;
-                    currentMark = markP2;
-                    break;
-                case markP2:
-                    chosenSquare.firstElementChild.textContent = markP2;
-                    currentMark = markP1;
-                    break;
+            if (currentMark === markP1 && !squareTextBox.textContent) {
+                squareTextBox.textContent = markP1;
+                gameBoard.populateBoard(square.id, markP1);
+                currentMark = markP2;
+            } else if (currentMark === markP2 && !squareTextBox.textContent) {
+                squareTextBox.textContent = markP2;
+                gameBoard.populateBoard(square.id, markP2);
+                currentMark = markP1;
             }
         })
     })
